91d5869fe82f0896a583501286d6b6b4ee85fd00
Chaitanyatraining <chaithanya5421@gmail.com>
2023-11-21 09:41:47 +0530

    form-validation

---
+++ formValidation/script.js
@@ -0,0 +1,32 @@
+//selecting input fields
+
+// nameValue.addEventListener('keyup', ()=>{
+//     console.log(nameValue.value)
+// })
+
+const nameValue = document.getElementById('name-value')
+const emailValue = document.getElementById('email-value')
+const pwdValue = document.getElementById('pwd-value')
+const phoneValue = document.getElementById('phone-value')
+const genderValue = document.getElementById('gender-value')
+const occValue = document.getElementById('occ-value')
+const nameErr = document.getElementById('name-Err')
+const addBtn = document.getElementById('addBtn')
+
+//function validating name field
+function validateName(){
+    const name = nameValue.value
+
+    if(name.length == 0){
+        nameErr.innerHTML = `<span class="text-danger" id="name-Err">Enter a valid userName</span>`
+        addBtn.disabled = true;
+
+        return false;
+    }
+
+    nameErr.innerHTML = `<span class="text-success" id="name-Err">Valid Username</span>`
+    addBtn.disabled = false;
+    return true
+}
+
+nameValue.addEventListener('keyup', validateName)
